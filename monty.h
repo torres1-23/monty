a6f4b9466a4b8542270a72238b5179b7a1708843
torres1-23 <70782680+torres1-23@users.noreply.github.com>
2020-12-22 04:41:10 +0000

    Add header file

---
+++ monty.h
@@ -0,0 +1,60 @@
+#ifndef _MONTY_H_
+#define _MONTY_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ *
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO Holberton project
+ */
+
+typedef struct stack_s
+{
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
+} stack_t;
+
+/**
+ * struct instruction_s - opcode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO Holberton project
+ */
+
+typedef struct instruction_s
+{
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
+
+/**
+ * struct argu_s - opcode and value
+ * @command: opcode
+ * @value: value
+ */
+
+typedef struct argu_s
+{
+	char *command;
+	char *value;
+} argu_t;
+extern argu_t arguments;
+argu_t arguments;
+
+void get_opcode(stack_t **head, unsigned int line_number);
+void _push(stack_t **head, unsigned int line_number);
+void _print_all(stack_t **head, unsigned int line_number);
+void free_stuff(char *buffer, stack_t *head);
+void errors(unsigned int code, char *argv, unsigned int n_line);
+
+#endif /* _MONTY_H */
