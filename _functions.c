51db6b50993e7529120078da427e221587608701
torres1-23 <70782680+torres1-23@users.noreply.github.com>
2020-12-22 04:42:23 +0000

    Add functions file

---
+++ _functions.c
@@ -0,0 +1,46 @@
+#include "monty.h"
+
+/**
+ * _push - push to stack
+ * @head: pointer to head node of stack
+ * @line_number: number of the line parsed.
+ */
+
+void _push(stack_t **head, unsigned int line_number)
+{
+	stack_t *tmp = *head, *new;
+
+	if (atoi(arguments.value) == 0 && (arguments.value)[0] != '0')
+		errors(3, NULL, line_number);
+	new = malloc(sizeof(stack_t));
+	if (!new)
+		errors(4, NULL, 0);
+	if (!*head)
+		new->next = NULL;
+	else
+	{
+		new->next = tmp;
+		tmp->prev = new;
+	}
+	new->n = atoi(arguments.value);
+	new->prev = NULL;
+	*head = new;
+}
+
+/**
+ * _print_all - print stack from top
+ * @head: pointer to head node of stack
+ * @line_number: number of the line parsed.
+ */
+
+void _print_all(stack_t **head, unsigned int line_number)
+{
+	stack_t *tmp = *head;
+
+	(void) line_number;
+	while (tmp)
+	{
+		printf("%d\n", tmp->n);
+		tmp = tmp->next;
+	}
+}
