60dbbacbd1b5c3b3fa14adb62e4ac8abdd1d509c
torres1-23 <70782680+torres1-23@users.noreply.github.com>
2020-12-22 04:41:38 +0000

    Add main file

---
+++ monty_main.c
@@ -0,0 +1,39 @@
+#include "monty.h"
+
+/**
+ * main - Entry point, file parsing.
+ * @argc: number of arguments.
+ * @argv: array of arguments.
+ * Return: 0 on success.
+ */
+
+int main(int argc, char **argv)
+{
+	char *buffer = NULL;
+	size_t buffersize = 0;
+	FILE *stream;
+	stack_t *head = NULL;
+	unsigned int line_number = 0;
+
+	if (argc != 2)
+	errors(0, NULL, 0);
+	arguments.command = NULL;
+	arguments.value = NULL;
+	stream = fopen(argv[1], "r");
+	if (!stream)
+		errors(1, argv[1], 0);
+	while (getline(&buffer, &buffersize, stream) != -1)
+	{
+		line_number++;
+		if (*buffer == '\n')
+			continue;
+		arguments.command = strtok(buffer, "\n ");
+		if (!arguments.command || *arguments.command == '#')
+			continue;
+		arguments.value = strtok(NULL, "\n ");
+		get_opcode(&head, line_number);
+	}
+	free_stuff(buffer, head);
+	fclose(stream);
+	return (0);
+}
